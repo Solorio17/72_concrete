43847750b9f06107b1fc0f841b75c7bee6a3c767
jason solorio <jasonsoloriom@gmail.com>
2020-05-23 22:44:03 -0600

    file cleanup, addition of material ui theming, and starting a test theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import useStyles from './App.styled';
+
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className={classes.root}>Button</Button>
     </div>
-  );
+  )
 }
 
 export default App;
